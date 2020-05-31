5f8c5759d6913dbed076e91e48c268c483aa9f76
Bobo <Bobo@Bobo-MBA.local>
2020-05-31 23:12:57 +0800

    Feat: eval_metric turn to aucpr, save model

--- model.py
+++ model.py
@@ -2,26 +2,27 @@
 from xgboost.sklearn import XGBClassifier
 import pickle
 
-def model(trX, trY, vaX, vaY, model_path='./model/xgb.pkl'):
-    clf = XGBClassifier()
-    eval_set = [(trX, trY), (valX, valY)]
+def xgb_model(trX, trY, vaX, vaY, model_path='./model/xgb.pkl'):
+    clf = XGBClassifier(n_estimators=50, scale_pos_weight=10)
+    eval_set = [(trX, trY), (vaX, vaY)]
     clf.fit(trX, trY, 
             early_stopping_rounds=10, 
             eval_set=eval_set, 
-            eval_metric='auc', 
-            scale_pos_weight=10,
+            eval_metric='aucpr', 
             verbose=True)
-    evals_result = clf.evals_result()
+
+    save_model(clf, model_path)
+    return clf
 
 def predict(model_path, features):
     loaded_model = load_model(model_path)
     y_pred = loaded_model.predict(features)
 
 def load_model(model_path):
-    loaded_model = pickle.load(model_path, 'rb')) 
+    loaded_model = pickle.load(model_path, 'rb')
     return loaded_model
 
-def save_model(model_path):
+def save_model(clf, model_path):
     if os.path.exists('./model'):
         os.makedirs('./model')
     pickle.dump(clf, open(model_path, 'wb'))
