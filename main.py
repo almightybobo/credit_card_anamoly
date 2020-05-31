ed24a1356895d9a00886bb8f49800d19b8a5d2fb
Bobo <Bobo@Bobo-MBA.local>
2020-05-31 23:13:27 +0800

    Feat: main script, can train now

---
+++ main.py
@@ -0,0 +1,9 @@
+# main script
+import data_preprocess
+import model
+
+if __name__ == '__main__':
+    df = data_preprocess.read_csv_to_df('./data/train.csv')
+    df = data_preprocess.preprocess_data(df)
+    trX, trY, vaX, vaY = data_preprocess.split_train_valid(df)
+    clf = model.xgb_model(trX, trY, vaX, vaY)
