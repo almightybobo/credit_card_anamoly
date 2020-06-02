86b58732dbe8c3004c5f1b804046ddbb5e8d791b
Bobo <Bobo@Bobo-MBA.local>
2020-06-02 08:25:36 +0800

    Feat: model save/load, performance: val_aucpr=0.65747

--- model.py
+++ model.py
@@ -1,29 +1,33 @@
 ## run model and predict
 from xgboost.sklearn import XGBClassifier
 import pickle
+import os
 
-def xgb_model(trX, trY, vaX, vaY, model_path='./model/xgb.pkl'):
-    clf = XGBClassifier(n_estimators=50, scale_pos_weight=10)
+def xgb_model(trX, trY, vaX, vaY, model_path='./model/xgb'):
+    clf = XGBClassifier(n_estimators=100, scale_pos_weight=10)
     eval_set = [(trX, trY), (vaX, vaY)]
     clf.fit(trX, trY, 
             early_stopping_rounds=10, 
             eval_set=eval_set, 
             eval_metric='aucpr', 
+            xgb_model=model_path if os.path.exists(model_path) else None,
             verbose=True)
 
-    save_model(clf, model_path)
+    clf.save_model(model_path)
     return clf
 
 def predict(model_path, features):
-    loaded_model = load_model(model_path)
-    y_pred = loaded_model.predict(features)
+    clf = load_model(model_path)
+    y_pred = clf.predict(features)
+    return y_pred
 
 def load_model(model_path):
-    loaded_model = pickle.load(model_path, 'rb')
-    return loaded_model
+    clf = XGBClassifier()
+    clf = clf.load_model(model_path)
+    return clf
 
 def save_model(clf, model_path):
-    if os.path.exists('./model'):
+    if not os.path.exists('./model'):
         os.makedirs('./model')
     pickle.dump(clf, open(model_path, 'wb'))
     print('Model saved!')
