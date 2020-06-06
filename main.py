3fa951a1a2f9ef264ddec16a101cb6b5f000fc08
Bobo <Bobo@59-102-252-117.nty.dynamic.tbcnet.net.tw>
2020-06-06 11:19:45 +0800

    Test: small feature set on colab

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     df = data_preprocess.read_csv_to_df(args.data)
-    df = data_preprocess.preprocess_data2(df)
+    df = data_preprocess.preprocess_data1(df)
     trX, trY, vaX, vaY = data_preprocess.split_train_valid(df)
     clf = model.xgb_model(trX, trY, vaX, vaY, model_path=args.model)
