195fd69c43f47f17aa353a4f185d19954b7cca70
Bobo <Bobo@59-102-252-117.nty.dynamic.tbcnet.net.tw>
2020-06-06 11:39:21 +0800

    Test: edit print in main

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--model', default='', type=str)
     args = parser.parse_args()
 
+    print('read data')
     df = data_preprocess.read_csv_to_df(args.data)
+    print('data preprocess')
     df = data_preprocess.preprocess_data2(df)
+    print('train')
     trX, trY, vaX, vaY = data_preprocess.split_train_valid(df)
     clf = model.xgb_model(trX, trY, vaX, vaY, model_path=args.model)
